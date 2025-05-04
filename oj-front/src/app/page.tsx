4e50be15b18457ccff2a827719d3bf8780a94591
Aline <113183673+Aline875@users.noreply.github.com>
2025-05-04 20:30:13 -0300

    feat: add cadastro and home page + components

--- oj-front/src/app/page.tsx
+++ oj-front/src/app/page.tsx
@@ -5,18 +5,18 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function Home() {
+export default function Login() {
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
       <main className="flex flex-col md:flex-row w-full h-full min-h-screen">
-        {/* Lado esquerdo: Logo */}
+        
         <div className="w-full md:w-1/2 bg-[#E8E8E7] flex items-center justify-center p-10">
           <h1 className="text-4xl font-extrabold bg-gradient-to-r from-[#D9D9D9] to-[#5E52FF] bg-clip-text text-transparent text-center">
             OrientaJovem
           </h1>
         </div>
 
-        {/* Lado direito: Formulário com gradiente vertical (bottom to top) */}
+        
         <div className="w-full md:w-1/2 bg-gradient-to-t from-[#1A5579] to-[#2A2570] flex items-center justify-center p-10">
           <form className="w-full max-w-md flex flex-col gap-6 text-white items-center">
             <h2 className="text-2xl font-semibold text-center">Login</h2>
---
+++ oj-front/src/components/Header.tsx
@@ -0,0 +1,13 @@
+// Header component
+const Header = () => {
+    return (
+      <header className="w-full bg-[#323536] bg-opacity-50 py-4 px-6 flex items-center">
+        <h1 className="text-4xl font-semibold bg-gradient-to-r from-[#D9D9D9] to-[#5E52FF] bg-clip-text text-transparent">
+          OrientaJovem
+        </h1>
+      </header>
+    );
+  };
+  
+  export default Header;
+  
